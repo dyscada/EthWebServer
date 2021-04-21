f60ddce2a27c9c95eaa8d69f512c321eefc7601e
dyscada <dyscada@gmail.com>
2021-04-21 16:50:19 +0900

    JS Navbar

--- data/script.js
+++ data/script.js
@@ -6,6 +6,18 @@
   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
+"use strict";
+//Make navbar transparent when it is on the top
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
+
 let scene, camera, rendered, cube;
 
 function parentWidth(elem) {
@@ -145,7 +157,6 @@ if (!!window.EventSource) {
     },
     false
   );
-
 }
 
 function resetPosition(element) {
